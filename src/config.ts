2c4e41876572b8391b9d534eab248d0844e8c95a
Vo Van Nghia <git@vnghia.com>
2026-03-15 17:44:19 +0100

    fix: overhaul info

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Vo Van Nghia",
+  title: "Software Engineer & Homelab Enthusiast & Wannabe Scientist",
+  description: "Portfolio website of Vo Van Nghia",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "contact@vnghia.com",
+    linkedin: "https://linkedin.com/in/vnghia",
+    github: "https://github.com/vnghia",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
